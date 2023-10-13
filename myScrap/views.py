5240cb153c45346c1accd9a342ada1ffa9fe43bf
AbhiMisRaw <sumitmm9506555798@gmail.com>
2023-10-13 11:51:46 -0700

    build a simple link scrapper by using BeautifulSoup

---
+++ myScrap/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+import requests as rq
+
+from bs4 import BeautifulSoup
+
+from .models import Link
+
+from django.http import HttpResponseRedirect
+# Create your views here.
+
+def scarpe(requests):
+    if requests.method =="POST":
+        site = requests.POST.get('site','')
+        page = rq.get(site)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        
+        for link in soup.find_all('a'):
+            link_address = link.get('href')
+            link_text = link.string
+            Link.objects.create(address = link_address, name = link_text )
+        return HttpResponseRedirect('/')
+    else :
+        data = Link.objects.all()
+    
+    return render(requests , 'myScrap/listlink.html' , {'data' : data } )
+
+def clear(requests):
+    Link.objects.all().delete()
+    return render(requests , 'myScrap/listlink.html')
